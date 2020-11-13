1ffdb12794ce0e252c7ab902c9dcfd32447e0498
Agnieszka Brown <abrown2012@icloud.com>
2020-11-12 21:34:22 -0500

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,26 @@
+class Song
+    attr_accessor :artist, :name
+  
+    @@all = []
+  
+    def initialize(name)
+      @name = name
+      
+      save
+    end
+  
+    def save
+      @@all << self
+    end
+  
+    def self.all
+      @@all
+    end
+
+    def self.new_by_filename(filename)
+        song = self.new
+        song.title = filename.split(" - ")[1]
+        song
+    end
+
+  end 
